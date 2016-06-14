880d4cc5319b78e2b57e57f31888250172e55b2a
Lee <xbc1207a@gmail.com>
2016-06-14 18:10:43 +0800

    initialize the transfer matrix and start state

--- main.cpp
+++ main.cpp
@@ -5,10 +5,19 @@ using namespace std;
 
 const int N=3;
 
-double start[N];
+double start[N]={0.33,0.33,0.34};
 double a[N][N];
 
 int main()
 {
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        {
+            if( i==j ) a[i][j]=0.34;
+            else a[i][j]=0.33;
+        }
+    }
+
     return 0;
 }
